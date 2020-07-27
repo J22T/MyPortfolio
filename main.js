a5e15171176241cfbe52730df4cb1d23a2f293e2
J22T <66581302+J22T@users.noreply.github.com>
2020-07-26 23:01:00 -0700

    commit

--- main.js
+++ main.js
@@ -3,26 +3,7 @@ import {planets} from './planets.js';
 
 films.map()
 
-function createAuthor(author){
-    let li = createNode('li'),
-    img = createNode('img'),
-    span = createNode('span');
 
-    img.src = author.picture.medium;
-    span.innerHTML = `${author.name.first} ${author.name.last}`;
-
-    append(li, img);
-    append(li, span);
-    append(ul, li);
-}
-
-function createNode(element) {
-    return document.createElement(element);
-}
-
-function append(parent, el) {
-    return parent.appendChild(el);
-}
 
 const url = starships.js;
 const ul = document.getElementById('authors');
@@ -40,4 +21,9 @@ fetch(url)
         authors.map(author => {
             createAuthor(author)
         })
+    })
+
+    genderToggleBtn.addEventListener('click',() => {
+        isStarship = !isStarship;
+        ul.innerHTML = '';
     }
