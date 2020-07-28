d7a4561b65fb2be1e787ef39d822862a8ff7b88f
J22T <66581302+J22T@users.noreply.github.com>
2020-07-27 22:04:54 -0700

    commit

--- main.js
+++ main.js
@@ -3,31 +3,7 @@
 import {planets} from './planets.js'
 import {starships} from './starships.js'
 
-console.log(planets);
 
-//get a pointer to the div with an id of planets in the HTML file
-const planetsDiv = document.querySelector('#planets');
-
-// take the planets array and run a function for each item in the array
-planets.map(function planetsFunc(planet){
-
-    //for each planet, create a div element
-    const div = document.createElement('div');
-    const p = document.createElement('p');
-
-    //add a class name for the divs
-    div.className = 'planet' 
-
-    //then, take the name and put it inside the div
-    //<div>alderan</div>
-    // div.innerHTML = planet.name;
-    p.innerHTML = planet.name;
-    // div.innerHTML = `Planet name ${planet.name} Planet size: ${planet.diameter}`;
-
-    div.appendChild(p);
-    //Attach the div with the planet to the HTML
-    planetsDiv.appendChild(div);
-})
 
 
 
