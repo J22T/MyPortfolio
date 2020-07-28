9f64f6fbb995d38d0d8e809fd6b124de37e724e6
J22T <66581302+J22T@users.noreply.github.com>
2020-07-27 22:29:22 -0700

    commit

--- main.js
+++ main.js
@@ -4,63 +4,55 @@ import {planets} from './planets.js'
 import {starships} from './starships.js'
 
 console.log(planets);
-console.log(starships);
 
 //get a pointer to the div with an id of planets in the HTML file
 const planetsDiv = document.querySelector('#planets');
-const planetsDiv = document.querySelector('#starships');
 
 // take the planets array and run a function for each item in the array
 planets.map(function planetsFunc(planet){
-starships.map(function starshipsFunc(starship){
 
     //for each planet, create a div element
     const div = document.createElement('div');
     const p = document.createElement('p');
-    const div = document.createElement('div');
-    const p = document.createElement('p');
 
     //add a class name for the divs
-    div.className = 'planet'
-    div.className = 'starship'
+    div.className = 'planet' 
 
     //then, take the name and put it inside the div
     //<div>alderan</div>
     // div.innerHTML = planet.name;
     p.innerHTML = planet.name;
-    p.innerHTML = starship.name;
     // div.innerHTML = `Planet name ${planet.name} Planet size: ${planet.diameter}`;
 
     div.appendChild(p);
-    div.appendChild(p);
     //Attach the div with the planet to the HTML
     planetsDiv.appendChild(div);
-    starshipDiv.appendChild(div);
 })
 
 
 
+console.log(starships);
+
+const planetsDiv = document.querySelector('#starships');
+
+starships.map(function starshipsFunc(starship){
+
+const div = document.createElement('div');
+const p = document.createElement('p');
+
+div.className = 'starship'
+
+p.innerHTML = starship.name;
+
+div.appendChild(p);
+
+starshipDiv.appendChild(div);
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+}
 
 
 
